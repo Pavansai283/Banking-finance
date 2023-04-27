e95951174d2423ab08ec5bed8392bd6c606cfc16
pavan sai <pavan.sai283@gmail.com>
2023-04-27 14:14:26 +0530

    updated

--- my-serverfiles/main.tf
+++ my-serverfiles/main.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "test-server" {
+resOOBource "aws_instance" "test-server" {
   ami           = "ami-02eb7a4783e7e9317" 
   instance_type = "t2.medium" 
   availability_zone = "ap-south-1a"
@@ -11,24 +11,16 @@ resource "aws_instance" "test-server" {
     private_key = file("myEC2key.pem")
     host     = self.public_ip
   }  
-  
+  provisioner "remote-exec" {
+    inline = [ "echo 'wait to start instance' "]
+  }
   tags = {
     Name = "test-server"
   }
-  
+  provisioner "local-exec" {
+        command = " echo ${aws_instance.test-server.public_ip} > inventory "
   }
-   provisioner "remote-exec" {
-     inline = [
-               "sudo apt upadte -y",
-               "sudo apt install docker.io -y",
-               "sudo snap install microk8s --clasic",
-               "sudo sleep 30",
-               "sudo microk8s status",
-               "sudo microk8s kubectl create deployment bankapp-deploy --image=pavanputtur/bankapp:1.0",
-               "sudo microk8s kubectl expose deployment bankapp-deploy --port=8084 --type=NodePort",
-               "sudo microk8s kubectl get svc",
-               "sudo echo public IP Address of the Instance",
-               "sudo curl http://checkip.amazonaws.com",
-       ]
-   } 
+   provisioner "local-exec" {
+  command = "ansible-playbook /var/lib/jenkins/workspace/bankapp/my-serverfiles/playbook.yml "
+  } 
 }
