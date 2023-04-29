ca4aa540838a47522a96c778387f833595e5cecf
pavan sai <pavan.sai283@gmail.com>
2023-04-29 11:15:46 +0530

    updated

--- my-serverfiles/main.tf
+++ my-serverfiles/main.tf
@@ -20,7 +20,4 @@ resource "aws_instance" "test-server" {
   provisioner "local-exec" {
         command = " echo ${aws_instance.test-server.public_ip} > inventory "
   }
-   provisioner "local-exec" {
-  command = "ansible-playbook /var/lib/jenkins/workspace/Bank_Project/my-serverfiles/playbook.yml "
-  } 
 }
