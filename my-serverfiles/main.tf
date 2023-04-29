a43d3ae088456e3c2bb75976875cf0625c2c1024
pavan sai <pavan.sai283@gmail.com>
2023-04-29 11:34:43 +0530

    updated

--- my-serverfiles/main.tf
+++ my-serverfiles/main.tf
@@ -4,22 +4,12 @@ resource "aws_instance" "test-server" {
   availability_zone = "ap-south-1a"
   vpc_security_group_ids= ["sg-0e3240a277ba0665b"]
   key_name = "mykey"
-  
-  connection {
-    type     = "ssh"
-    user     = "ubuntu"
-    private_key = file("mykey.pem")
-    host     = self.public_ip
-  }  
-  provisioner "remote-exec" {
-    inline = [ "echo 'wait to start instance' "]
-  }
   tags = {
     Name = "test-server"
   }
  // provisioner "local-exec" {
        // command = " echo ${aws_instance.test-server.public_ip} > inventory "
-  }
+  //}
   provisioner "remote-exec" {
      inline = [
       "sudo apt update -y",
